c013b1f64a32238cc36d8dc3505c6225e3df8d68
Andrew Borgman <borgmaan@mail.gvsu.edu>
2012-11-07 20:36:41 -0500

    server side bug2

--- gene_mapper/association.py
+++ gene_mapper/association.py
@@ -1,12 +1,12 @@
 # Association testing
-#from fisher import pvalue
-#from rpy2.robjects.packages import importr
-#import rpy2.robjects as robjects
-#from rpy2.robjects import FloatVector,StrVector
+from fisher import pvalue
+from rpy2.robjects.packages import importr
+import rpy2.robjects as robjects
+from rpy2.robjects import FloatVector,StrVector
 import math
 from itertools import chain
 from collections import Counter
-#import numpy as np
+import numpy as np
 from scipy import stats
 
 """
